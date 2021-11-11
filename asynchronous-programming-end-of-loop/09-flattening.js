5e1d12f4968b0012df6590e3f45f605b801da455
jalpajora <jezamae.alpajora@gmail.com>
2021-11-12 00:10:53 +0800

    study: 9 - flattening

---
+++ asynchronous-programming-end-of-loop/09-flattening.js
@@ -0,0 +1,81 @@
+var exchanges = [
+  {
+    name: 'NYSE',
+    stocks: [
+      {
+        symbol: 'XFX',
+        closes: [
+          { date: new Date(2014, 11, 24), price: 240.1 },
+          { date: new Date(2014, 11, 23), price: 232.08 },
+          { date: new Date(2014, 11, 22), price: 241.09 },
+        ],
+      },
+      {
+        symbol: 'TNZ',
+        closes: [
+          { date: new Date(2014, 11, 24), price: 240.1 },
+          { date: new Date(2014, 11, 23), price: 232.08 },
+          { date: new Date(2014, 11, 22), price: 241.09 },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'TSX',
+    stocks: [
+      {
+        symbol: 'JXJ',
+        closes: [
+          { date: new Date(2014, 11, 24), price: 521.24 },
+          { date: new Date(2014, 11, 23), price: 424.84 },
+          { date: new Date(2014, 11, 22), price: 419.72 },
+        ],
+      },
+      {
+        symbol: 'NYN',
+        closes: [
+          { date: new Date(2014, 11, 24), price: 16.82 },
+          { date: new Date(2014, 11, 23), price: 16.12 },
+          { date: new Date(2014, 11, 22), price: 15.77 },
+        ],
+      },
+    ],
+  },
+];
+
+Array.prototype.concatAll = function () {
+  var results = [];
+
+  this.forEach(function (subArray) {
+    subArray.forEach(function (item) {
+      results.push(item);
+    });
+  });
+
+  return results;
+};
+
+var christmasEveCloses = exchanges
+  .map(function (exchange) {
+    return exchange.stocks
+      .map(function (stock) {
+        return stock.closes
+          .filter(function (close) {
+            return close.date.getMonth() === 11 && close.date.getDate() === 24;
+          })
+          .map(function (close) {
+            return {
+              symbol: stock.symbol,
+              price: close.price,
+            };
+          });
+      })
+      .concatAll();
+  })
+  .concatAll();
+
+christmasEveCloses.forEach(function (christmasEveClose) {
+  console.log(christmasEveClose);
+});
+
+console.log(christmasEveCloses);
