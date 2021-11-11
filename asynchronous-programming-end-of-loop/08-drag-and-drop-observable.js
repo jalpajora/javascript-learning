c88306a30af9853e7a86583beb278334a14bdfc1
jalpajora <jezamae.alpajora@gmail.com>
2021-11-11 23:49:29 +0800

    study: 8 - drag and drop

--- asynchronous-programming-end-of-loop/08-drag-and-drop-observable.js
+++ asynchronous-programming-end-of-loop/08-drag-and-drop-observable.js
@@ -1,2 +1,19 @@
 var parent = document.getElementById('parent');
-var widget = ocument.getElementById('widget');
+var widget = document.getElementById('widget');
+
+var mouseDowns = rxjs.fromEvent(widget, 'mousedown');
+var parentMouseMoves = rxjs.fromEvent(parent, 'mousemove');
+var parentMouseUps = rxjs.fromEvent(parent, 'mouseup');
+
+var drags = mouseDowns.pipe(
+  rxjs.operators.map(function (e) {
+    return parentMouseMoves.pipe(rxjs.operators.takeUntil(parentMouseUps));
+  }),
+  rxjs.operators.concatAll()
+);
+
+var subscription = drags.forEach(function (e) {
+  // console.log(e);
+  widget.style.left = e.clientX + 'px';
+  widget.style.top = e.clientY + 'px';
+});
